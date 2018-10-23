a448c2b731542f27ac44c9125d250fba4a9f0107
Rachel <rmarincola@gmail.com>
2018-10-23 12:41:16 +0300

    new models and migration file.

--- airqo_monitor/models.py
+++ airqo_monitor/models.py
@@ -1,5 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Incident(models.Model):
+
+    class Meta:
+        db_table = 'incident'
+
+    created_at = models.DateTimeField("date created", auto_now_add=True)
+    channel_id = models.IntegerField(null=False, db_index=True)
+
+    def malfunction_reasons(self):
+        return IncidentMalfunctionReasonLink.objects.filter(incident=self).all()
+
+
+class MalfunctionReason(models.Model):
+
+    class Meta:
+        db_table = 'malfunction_reason'
+
+    name = models.TextField(null=False)
+    description = models.TextField(null=False)
+
+    def incidents(self):
+        return IncidentMalfunctionReasonLink.objects.filter(malfunction_reason=self)
+
+
+class IncidentMalfunctionReasonLink(models.Model):
+
+    class Meta:
+        db_table = 'incident_malfunction_reason_link'
+        unique_together = ('incident', 'malfunction_reason')
+
+    incident = models.ForeignKey(
+        Incident,
+        null=False,
+        db_index=True,
+        on_delete=models.DO_NOTHING,
+    )
+    malfunction_reason = models.ForeignKey(
+        MalfunctionReason,
+        null=False,
+        db_index=True,
+        on_delete=models.DO_NOTHING,
+    )
