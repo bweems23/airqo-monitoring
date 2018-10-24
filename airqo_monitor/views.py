a0f50114fc82bb7a41c8bbf711858eab63daeab2
Rachel <rmarincola@gmail.com>
2018-10-24 12:18:43 +0300

    remove comments and unused file.

--- airqo_monitor/views.py
+++ airqo_monitor/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 from airqo_monitor.external.thingspeak import get_all_channel_ids
 from airqo_monitor.get_malfunctions import get_all_channel_malfunctions_cached
 
-# Create your views here.
+
 def index(request):
-    # return HttpResponse('Hello from Python!')
     malfunctioning_channels = get_all_channel_malfunctions_cached()
 
     return render(
