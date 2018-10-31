891158f11bce9f7183d91f7f3bd07fdac370e305
Rachel <rmarincola@gmail.com>
2018-10-31 13:49:36 +0300

    register our models to django admin.

--- airqo_monitor/admin.py
+++ airqo_monitor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from airqo_monitor.models import (
+	Channel,
+	ChannelNote,
+	Incident,
+	IncidentMalfunctionReasonLink,
+	MalfunctionReason,
+)
+
 # Register your models here.
+admin.site.register(Channel)
+admin.site.register(ChannelNote)
+admin.site.register(Incident)
+admin.site.register(IncidentMalfunctionReasonLink)
+admin.site.register(MalfunctionReason)
