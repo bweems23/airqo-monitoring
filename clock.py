78e251aa5b036ab9d7e220c70fa4cf70a4fe290a
Rachel <rmarincola@gmail.com>
2018-11-02 11:42:28 +0300

    add schedule.

---
+++ clock.py
@@ -0,0 +1,15 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from airqo_monitor.get_malfunctions import (
+    get_all_channel_malfunctions
+)
+from airqo_monitor.models import ChannelNote
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=5)
+def update_channel_data():
+	ChannelNote.objects.create(channel_id=2, note='test note', author='rachel')
+    get_all_channel_malfunctions()
+
+sched.start()
