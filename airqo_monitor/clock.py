c8d16e02dd62de7bf30dd23cb477a8400620cb0d
Rachel <rmarincola@gmail.com>
2018-11-02 14:37:58 +0300

    new file.

--- airqo_monitor/clock.py
+++ airqo_monitor/clock.py
@@ -1,29 +1,26 @@
-import django
+# import django
 
-from django.conf import settings
+# from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from airqo_monitor.models import ChannelNote
+from airqo_monitor.get_malfunctions import (
+    get_all_channel_malfunctions
+)
 
-settings.configure()
-django.setup()
+
+# settings.configure()
+# django.setup()
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def update_channel_data_interval():
-    from airqo_monitor.models import ChannelNote
-    from airqo_monitor.get_malfunctions import (
-        get_all_channel_malfunctions
-    )
     ChannelNote.objects.create(channel_id=2, note='test note INTERVAL', author='rachel')
     get_all_channel_malfunctions()
 
 @sched.scheduled_job('cron', minute=5)
 def update_channel_data():
-    from airqo_monitor.models import ChannelNote
-    from airqo_monitor.get_malfunctions import (
-        get_all_channel_malfunctions
-    )
     ChannelNote.objects.create(channel_id=2, note='test note', author='rachel')
     get_all_channel_malfunctions()
 
