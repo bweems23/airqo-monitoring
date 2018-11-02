9263d66c00670b5c238987c9bc9fe31474ba13dc
Rachel <rmarincola@gmail.com>
2018-11-02 11:48:57 +0300

    do a cron one.

--- clock.py
+++ clock.py
@@ -8,8 +8,13 @@ from airqo_monitor.models import ChannelNote
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=5)
+def update_channel_data_interval():
+    ChannelNote.objects.create(channel_id=2, note='test note INTERVAL', author='rachel')
+    get_all_channel_malfunctions()
+
+@sched.scheduled_job('cron', minute=5)
 def update_channel_data():
-	ChannelNote.objects.create(channel_id=2, note='test note', author='rachel')
+    ChannelNote.objects.create(channel_id=2, note='test note', author='rachel')
     get_all_channel_malfunctions()
 
 sched.start()
