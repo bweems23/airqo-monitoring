4ee5d0cac8deb72dcdbd419863a3c983b6402f7d
Rachel <rmarincola@gmail.com>
2018-11-05 12:58:53 +0300

    try a scheduler insdie custom django command.

--- airqo_monitor/management/commands/run_scheduler.py
+++ airqo_monitor/management/commands/run_scheduler.py
@@ -1,5 +1,7 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management.base import BaseCommand, CommandError
 
+from airqo_monitor.models import ChannelNote
 from airqo_monitor.get_malfunctions import get_all_channel_malfunctions
 
 
@@ -7,6 +9,13 @@ class Command(BaseCommand):
     help = 'Runs all scheduler tasks'
 
     def handle(self, *args, **options):
-        print('Running scheduled tasks...')
-        get_all_channel_malfunctions()
-        print('Scheduled tasks complete.')
+        sched = BlockingScheduler()
+
+        @sched.scheduled_job('cron', minute='*')
+        def update_channel_data():
+            print('Running scheduled tasks...')
+            ChannelNote.objects.create(channel_id=2, note='test note clock try', author='rachel')
+            get_all_channel_malfunctions()
+            print('Scheduled tasks complete.')
+
+        sched.start()
