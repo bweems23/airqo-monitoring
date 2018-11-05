f4bf57d783139c0b990fc6755f3e2b8f7586aac8
Rachel <rmarincola@gmail.com>
2018-11-05 12:25:30 +0300

    remove import.

--- airqo_monitor/management/commands/run_scheduler.py
+++ airqo_monitor/management/commands/run_scheduler.py
@@ -1,9 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from airqo_monitor.models import ChannelNote
-from airqo_monitor.get_malfunctions import (
-    get_all_channel_malfunctions
-)
+from airqo_monitor.get_malfunctions import get_all_channel_malfunctions
 
 
 class Command(BaseCommand):
