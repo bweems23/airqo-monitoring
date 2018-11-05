59d1b9abb6ac409d119faab446026e643e6c0f68
Rachel <rmarincola@gmail.com>
2018-11-05 13:03:35 +0300

    remove tested note.

--- airqo_monitor/management/commands/run_scheduler.py
+++ airqo_monitor/management/commands/run_scheduler.py
@@ -1,7 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management.base import BaseCommand, CommandError
 
-from airqo_monitor.models import ChannelNote
 from airqo_monitor.get_malfunctions import get_all_channel_malfunctions
 
 
@@ -14,7 +13,6 @@ class Command(BaseCommand):
         @sched.scheduled_job('cron', minute='*')
         def update_channel_data():
             print('Running scheduled tasks...')
-            ChannelNote.objects.create(channel_id=2, note='test note clock try', author='rachel')
             get_all_channel_malfunctions()
             print('Scheduled tasks complete.')
 
