203a89f4524cec117940658db1f76b0e130a6f01
Rachel <rmarincola@gmail.com>
2018-11-06 16:55:03 +0300

    remove old functions.

--- airqo_monitor/get_malfunctions.py
+++ airqo_monitor/get_malfunctions.py
@@ -56,45 +56,6 @@ def _get_channel_malfunctions(channel_data, channel_type):
     return malfunction_list
 
 
-def _has_low_battery(channel_data, channel_type):
-    """Determine whether the channel has low battery. channel_data can't be empty."""
-    assert len(channel_data) > 0
-    if channel_type == AIRQO_CHANNEL_TYPE:
-        last_voltage = float(channel_data[-1].get('battery_voltage'))
-        return last_voltage < LOW_BATTERY_CUTOFF
-
-
-def _has_low_reporting_frequency(channel_data, channel_type):
-    """Determine whether the channel is reporting data at a lower frequency than expected."""
-    assert len(channel_data) > 0
-
-    if channel_type == AIRQO_CHANNEL_TYPE:
-        index_to_verify = min(len(channel_data), NUM_REPORTS_TO_VERIFY_REPORTING_MALFUNCTION)
-        report_to_verify = channel_data[-1 * index_to_verify]
-        report_timestamp = datetime.strptime(report_to_verify.get('created_at'), '%Y-%m-%dT%H:%M:%SZ')
-
-        # The cutoff time is now minus MINIMUM_REPORT_FREQUENCY_SECONDS seconds per report being evaluated.
-        # The number of reports being evaluated is determined by the index_to_verify.
-        cutoff_time = datetime.utcnow() - timedelta(seconds=MAXIMUM_AVERAGE_SECONDS_BETWEEN_REPORTS * index_to_verify)
-
-        # If the report timestamp is earlier than the cutoff time, that means that there is too much time passing
-        # between each point being reported.
-        return report_timestamp < cutoff_time
-
-
-def _sensor_is_reporting_outliers(channel_data, channel_type):
-    """Determine whether the sensor is reporting points outside the reasonable range.
-
-    Presence of outlier points may indicated an obstructed sensor.
-    """
-    assert len(channel_data) > 0
-    if channel_type == AIRQO_CHANNEL_TYPE:
-        num_points = min(NUM_REPORTS_TO_VERIFY_SENSOR_MALFUNCTION, len(channel_data))
-        is_outlier = lambda pm_2_5: pm_2_5 < SENSOR_PM_2_5_MIN_CUTOFF or pm_2_5 > SENSOR_PM_2_5_MAX_CUTOFF
-        extreme_reads = [entry for entry in channel_data[-1 * num_points:] if is_outlier(float(entry.get('pm_2_5')))]
-        return len(extreme_reads) > num_points * ALLOWABLE_OUTLIER_SENSOR_RATIO
-
-
 def update_db(channels):
     for channel in channels:
         channel_object = Channel.objects.filter(channel_id=channel["channel_id"]).first()
