53d9b13794e7806328de98f59683045b6eba9a83
Rachel <rmarincola@gmail.com>
2019-02-04 18:24:27 -0500

    time aware fix.

--- airqo_monitor/views.py
+++ airqo_monitor/views.py
@@ -148,15 +148,15 @@ def heatmap(request):
 
     start_time_str = queries.get('start_time', None)
     if start_time_str:
-        cleaned_str = start_time_str[0].replace('%3A', ':')
-        start_time = datetime.strptime(cleaned_str,'%Y-%m-%dT%H:%M').astimezone(local_tz)
+        cleaned_str = start_time_str[0].replace('%3A', ':') + ':00Z'
+        start_time = datetime.strptime(cleaned_str,'%Y-%m-%dT%H:%M:%SZ')
     else:
         start_time = datetime.utcnow() - timedelta(days=1)
 
     end_time_str = queries.get('end_time', None)
     if end_time_str:
-        cleaned_str = end_time_str[0].replace('%3A', ':')
-        end_time = datetime.strptime(cleaned_str,'%Y-%m-%dT%H:%M').astimezone(local_tz)
+        cleaned_str = end_time_str[0].replace('%3A', ':') + ':00Z'
+        end_time = datetime.strptime(cleaned_str,'%Y-%m-%dT%H:%M:%SZ')
 
     channel_ids_str = queries.get('channel_ids', None)
     if channel_ids_str:
